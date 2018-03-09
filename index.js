79816f0b652d6632e000d350556854919724495d
anjuthomas <anjuthomas@google.com>
2018-03-09 14:59:52 -0800

    Blank repo

--- index.js
+++
@@ -1,5 +0,0 @@
-/**
- * @fileoverview Node JS Library for Message protocols 
- * 
- * @description This is a module that implements the protocol messages in OAuth2 and OpenID Connect.
- */
--- src/oicMsg/index.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  verify: require('./verify'),
-  sign: require('./sign'),
-  JsonWebTokenError: require('../../lib/JsonWebTokenError'),
-  NotBeforeError: require('../../lib/NotBeforeError'),
-  TokenExpiredError: require('../../lib/TokenExpiredError'),
-};
