05f86cf8a113dcaed5034f24e0a08061bb972443
anjuthomas <anjuthomas@google.com>
2018-06-01 07:31:46 -0700

    Revised test

--- test/basicId-claims-test.js
+++ test/basicId-claims-test.js
@@ -1,14 +1,13 @@
-var fs = require('fs');
-var path = require('path');
-var assert = require('chai').assert;
-
-var BasicIdToken = require('../src/oicMsg/tokenProfiles/basicIdToken');
+const fs = require('fs');
+const path = require('path');
+const assert = require('chai').assert;
+const BasicIdToken = require('../src/oicMsg/tokenProfiles/basicIdToken');
 
 function loadKey(filename) {
   return fs.readFileSync(path.join(__dirname, filename));
 }
 
-var algorithms = {
+let algorithms = {
   RS256: {
     pub_key: loadKey('pub.pem'),
     priv_key: loadKey('priv.pem'),
@@ -27,27 +26,26 @@ var algorithms = {
 describe('Asymmetric Algorithms', function() {
   Object.keys(algorithms).forEach(function(algorithm) {
     describe(algorithm, function() {
-      var clockTimestamp = 1000000000;
+      let clockTimestamp = 1000000000;
 
       describe(
-          'when signing a basic id token with a known non standard claim',
-          function() {
-            var basicIdToken2 = new BasicIdToken({
-              iss: 'issuer',
-              sub: 'subject',
-              iat: clockTimestamp,
-              jti: 'jti'
-            });
-            basicIdToken2.addOptionalClaims({
-              'aud': 'audience',
-              'nbf': clockTimestamp + 2,
-              'exp': clockTimestamp + 3
-            });
-            basicIdToken2.setNoneAlgorithm(true);
+        'when signing a basic id token with a known non standard claim', function() {
+          let basicIdToken2 = new BasicIdToken({
+            iss: 'issuer',
+            sub: 'subject',
+            iat: clockTimestamp,
+            jti: 'jti'
+          });
+          basicIdToken2.addOptionalClaims({
+            'aud': 'audience',
+            'nbf': clockTimestamp + 2,
+            'exp': clockTimestamp + 3
+          });
+          basicIdToken2.setNoneAlgorithm(true);
 
-            it('should check known non standard claim', function(done) {
-              basicIdToken2.toJWT('shhhh').then(function(signedJWT) {
-                try {
+          it('should check known non standard claim', function(done) {
+             basicIdToken2.toJWT('shhhh').then(function(signedJWT) {
+               try {
                   let decodedPayload = basicIdToken2.fromJWT(
                       signedJWT, 'shhhh', {
                         'iss': 'issuer',
@@ -62,14 +60,14 @@ describe('Asymmetric Algorithms', function() {
                   assert.isNull(err);
                 }
                 done();
-              });
-            });
-
-            it('should throw when invalid known non standard claim',
-               function(done) {
-                 basicIdToken2.toJWT('shhhh').then(function(signedJWT) {
-                   try {
-                     let decodedPayload = basicIdToken2.fromJWT(
+            });            
+          });
+          
+          it('should throw when invalid known non standard claim',
+              function(done) {
+                basicIdToken2.toJWT('shhhh').then(function(signedJWT) {
+                  try {
+                    let decodedPayload = basicIdToken2.fromJWT(
                          signedJWT, 'shhhh', {
                            'iss': 'issuer',
                            'sub': 'subject',
@@ -82,143 +80,136 @@ describe('Asymmetric Algorithms', function() {
                    } catch (err) {
                      assert.isNotNull(err);
                    }
-                   done();
-                 });
-               });
+                   done(); 
+              });
           });
+      });
+        
+    describe('when signing a token without standard claim', function() {
+      it('should throw error and require standard claim', function(done) {
+        try {
+          let basicIdToken = new BasicIdToken({iss: 'issuer', sub: 'subject'});
+          basicIdToken.addOptionalClaims({'jti': 'test'});
+          basicIdToken.setNoneAlgorithm(true);
+          basicIdToken.toJWT('shhhh');
+        } catch (err) {
+          assert.isNotNull(err);
+          assert.instanceOf(err, Error);
+        }
+        done();
+      });
+    });
 
+    describe('when adding claims to token profile', function() {
+      let clockTimestamp = 1000000000;
 
-      describe('when signing a token without standard claim', function() {
-        it('should throw error and require standard claim', function(done) {
-          try {
-            var basicIdToken =
-                new BasicIdToken({iss: 'issuer', sub: 'subject'});
-            basicIdToken.addOptionalClaims({'jti': 'test'});
-            basicIdToken.setNoneAlgorithm(true);
-            basicIdToken.toJWT('shhhh');
-          } catch (err) {
-            assert.isNotNull(err);
-            assert.instanceOf(err, Error);
-          }
-          done();
-        });
+      let basicIdToken2 = new BasicIdToken({
+        'iss': 'issuer',
+        'sub': 'subject',
+        'iat': clockTimestamp,
+        'jti': 'jti'
+      });
+      basicIdToken2.addOptionalClaims({
+        'aud': 'audience',
+        'nbf': clockTimestamp + 2,
+        'exp': clockTimestamp + 3
+      });
+      basicIdToken2.setNoneAlgorithm(true);
+
+      it('should be able to access all standard claims', function(done) {
+        try {
+          let standardClaims = basicIdToken2.getRequiredClaims();
+          assert.deepEqual(standardClaims, {
+            'iss': 'issuer',
+            'sub': 'subject',
+            'iat': clockTimestamp,
+            'jti': 'jti'
+          });
+        } catch (err) {
+          assert.isNull(err);
+        }
+        done();
       });
 
-      describe('when adding claims to token profile', function() {
-        var clockTimestamp = 1000000000;
+      it('should be able to access non standard claims separately',
+         function(done) {
+           try {
+             let nonStandardClaims = basicIdToken2.getOptionalClaims();
+             assert.deepEqual(nonStandardClaims, {
+               'aud': 'audience',
+               'nbf': clockTimestamp + 2,
+               'exp': clockTimestamp + 3
+             });
+           } catch (err) {
+             assert.isNull(err);
+           }
+           done();
+         });
+    });
 
-        var basicIdToken2 = new BasicIdToken({
-          'iss': 'issuer',
-          'sub': 'subject',
-          'iat': clockTimestamp,
-          'jti': 'jti'
-        });
-        basicIdToken2.addOptionalClaims({
+    describe('when signing a token with standard claim', function() {
+      let signedJWT = null;     
+      let basicIdToken = null; 
+      let clockTimestamp = 1000000000;
+
+      it('should check standard claim', function(done) {
+        basicIdToken = new BasicIdToken(
+          {iss: 'issuer', sub: 'subject', iat: clockTimestamp, jti: 'jti'});
+        basicIdToken.addOptionalClaims({
           'aud': 'audience',
           'nbf': clockTimestamp + 2,
           'exp': clockTimestamp + 3
         });
-        basicIdToken2.setNoneAlgorithm(true);
-
-        it('should be able to access all standard claims', function(done) {
-          try {
-            var standardClaims = basicIdToken2.getRequiredClaims();
-            assert.deepEqual(standardClaims, {
-              'iss': 'issuer',
-              'sub': 'subject',
-              'iat': clockTimestamp,
-              'jti': 'jti'
-            });
-          } catch (err) {
-            assert.isNull(err);
-          }
-          done();
+        basicIdToken.setNoneAlgorithm(true);
+        basicIdToken.toJWT('shhhh').then(function(jws) {
+          signedJWT = jws;
+          basicIdToken
+          .fromJWT(
+              signedJWT, 'shhhh', {
+                'iss': 'issuer',
+                'sub': 'subject',
+                'maxAge': '1d',
+                'clockTolerance': 10,
+                'aud': 'audience',
+                'jti': 'jti'
+              },
+              {'clockTimestamp': clockTimestamp})
+          .then(function(decodedPayload) {
+            assert.isNotNull(decodedPayload);
+            done();              
+          });
         });
-
-        it('should be able to access non standard claims separately',
-           function(done) {
-             try {
-               var nonStandardClaims = basicIdToken2.getOptionalClaims();
-               assert.deepEqual(nonStandardClaims, {
-                 'aud': 'audience',
-                 'nbf': clockTimestamp + 2,
-                 'exp': clockTimestamp + 3
-               });
-             } catch (err) {
-               assert.isNull(err);
-             }
-             done();
-           });
       });
 
-      describe('when signing a token with standard claim', function() {
-        let signedJWT = null;
-        let basicIdToken = null;
-        let clockTimestamp = 1000000000;
+      it('should throw when invalid standard claim', function(done) {
 
-        it('should check standard claim', function(done) {
-          basicIdToken = new BasicIdToken(
-              {iss: 'issuer', sub: 'subject', iat: clockTimestamp, jti: 'jti'});
-          basicIdToken.addOptionalClaims({
-            'aud': 'audience',
-            'nbf': clockTimestamp + 2,
-            'exp': clockTimestamp + 3
+        basicIdToken = new BasicIdToken(
+          {iss: 'issuer', sub: 'subject', iat: clockTimestamp, jti: 'jti'});
+      basicIdToken.addOptionalClaims({
+        'aud': 'audience',
+        'nbf': clockTimestamp + 2,
+        'exp': clockTimestamp + 3
+      });
+      basicIdToken.setNoneAlgorithm(true);
+      basicIdToken.toJWT('shhhh').then(function(jws) {
+        signedJWT = jws;
+        basicIdToken
+            .fromJWT(
+                signedJWT, 'shhhh', {
+                  'iss': 'wrong-issuer',
+                  'sub': 'subject',
+                  'aud': 'audience',
+                  'maxAge': '1d',
+                  'clockTolerance': 10,
+                  'jti': 'jti'
+                },
+                {'clockTimestamp': clockTimestamp})
+            .catch(function(err) {
+              assert.isNotNull(err);
+              assert.equal(err.name, 'JsonWebTokenError');
+              done();              
+            });
           });
-          basicIdToken.setNoneAlgorithm(true);
-          basicIdToken.toJWT('shhhh').then(function(jws) {
-            signedJWT = jws;
-            basicIdToken
-                .fromJWT(
-                    signedJWT, 'shhhh', {
-                      'iss': 'issuer',
-                      'sub': 'subject',
-                      'maxAge': '1d',
-                      'clockTolerance': 10,
-                      'aud': 'audience',
-                      'jti': 'jti'
-                    },
-                    {'clockTimestamp': clockTimestamp})
-                .then(function(decodedPayload) {
-                  assert.isNotNull(decodedPayload);
-                })
-                .catch(function(err) {
-                  assert.isNull(err);
-                });
-            done();
-          })
-          done();
-        });
-
-        it('should throw when invalid standard claim', function(done) {
-          basicIdToken = new BasicIdToken(
-              {iss: 'issuer', sub: 'subject', iat: clockTimestamp, jti: 'jti'});
-          basicIdToken.addOptionalClaims({
-            'aud': 'audience',
-            'nbf': clockTimestamp + 2,
-            'exp': clockTimestamp + 3
-          });
-          basicIdToken.setNoneAlgorithm(true);
-          basicIdToken.toJWT('shhhh').then(function(jws) {
-            signedJWT = jws;
-            basicIdToken
-                .fromJWT(
-                    signedJWT, 'shhhh', {
-                      'iss': 'wrong-issuer',
-                      'sub': 'subject',
-                      'aud': 'audience',
-                      'maxAge': '1d',
-                      'clockTolerance': 10,
-                      'jti': 'jti'
-                    },
-                    {'clockTimestamp': clockTimestamp})
-                .catch(function(err) {
-                  assert.isNotNull(err);
-                  assert.equal(err.name, 'JsonWebTokenError');
-                  done();
-                });
-            done();
-          });
-          done();
         });
       });
     })
